8832ece63b304fefd2dcb38102a39030aeddbe0e
速水　達也 <t.hayamizu@outlook.com>
2020-09-04 11:50:31 +0900

    add CreateDummy.

---
+++ CreateDummyFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace CreateDummyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //argments
+            if (args.Length < 2)
+            {
+                System.Console.WriteLine("Usage:　CreateDummy [file-name] [file-size] ");
+                return;
+            }
+
+            var name = args[0];
+            var size = 0;
+            if (int.TryParse(args[1], out size) == false)
+            {
+                System.Console.WriteLine("数値で入力してください");
+                return;
+            }
+
+            //write
+            using (var stream = new StreamWriter($"./{name}"))
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    stream.Write(0x00);
+                }
+            }
+
+        }
+    }
+}
